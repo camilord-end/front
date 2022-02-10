0c13719db23a3a45f6cdd1080a8d74253d3a50ea
camilord-end <jcorozcoa@correo.udistrital.edu.co>
2022-02-10 18:59:19 -0500

    react start

---
+++ Certification/React/challenge.jsx
@@ -0,0 +1,27 @@
+//const JSX = <h1>Hello JSX!</h1>; 
+/*const JSX = 
+  <div>
+    <h1></h1>
+    <p></p>
+    <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+    </ul>
+  </div>; */
+
+//const JSX = (
+//    <div>
+//      <h1>This is a block of JSX</h1>
+//      <p>Here's a subtitle</p>
+//      {/* this is a comment */}
+//    </div>
+//);
+const JSX = (
+    <div>
+      <h1>Hello World</h1>
+      <p>Lets render this to the DOM</p>
+    </div>
+);
+// Change code below this line
+ReactDOM.render(JSX,document.getElementById("challenge-node"))
