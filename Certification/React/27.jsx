9d691a7519384dbfe6961291b53debbee3852b70
camilord-end <jcorozcoa@correo.udistrital.edu.co>
2022-02-21 14:51:02 -0500

    More Prog

--- Certification/React/27.jsx
+++ Certification/React/27.jsx
@@ -1,36 +1,33 @@
 class MyForm extends React.Component {
-    constructor(props) {
-      super(props);
-      this.state = {
-        input: '',
-        submit: ''
-      };
-      this.handleChange = this.handleChange.bind(this);
-      this.handleSubmit = this.handleSubmit.bind(this);
-    }
-    handleChange(event) {
-      this.setState({
-        input: event.target.value
-      });
-    }
-    handleSubmit(event) {
-      // Change code below this line
-  
-      // Change code above this line
-    }
-    render() {
-      return (
-        <div>
-          <form onSubmit={this.handleSubmit}>
-            {/* Change code below this line */}
-  
-            {/* Change code above this line */}
-            <button type='submit'>Submit!</button>
-          </form>
-          {/* Change code below this line */}
-  
-          {/* Change code above this line */}
-        </div>
-      );
-    }
-}
+  constructor(props) {
+    super(props);
+    this.state = {
+      input: '',
+      submit: ''
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  handleChange(event) {
+    this.setState({
+      input: event.target.value
+    });
+  }
+  handleSubmit(event) {
+    event.preventDefault()
+    this.setState({
+      submit: this.state.input
+    });
+  }
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input value={this.state.input} onChange={this.handleChange} />
+          <button type='submit'>Submit!</button>
+        </form>
+        <h1>{this.state.submit}</h1>
+      </div>
+    );
+  }
+};
---
+++ Certification/React/30.jsx
@@ -0,0 +1,11 @@
+class MyComponent extends React.Component {
+    constructor(props) {
+      super(props);
+    }
+    componentWillMount() {
+        console.log("Look at this console :o")
+    }
+    render() {
+      return <div />
+    }
+};
