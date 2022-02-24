16816fdbc8c4e6fa9f90364c97e3e30f9fc4242b
camilord-end <jcorozcoa@correo.udistrital.edu.co>
2022-02-24 11:08:28 -0500

    Practice 1

--- Certification/React+Redux/1.jsx
+++ Certification/React+Redux/1.jsx
@@ -1,5 +1,4 @@
 class DisplayMessages extends React.Component {
-  
   constructor(props){
     super(props);
     this.state = {
@@ -7,8 +6,33 @@ class DisplayMessages extends React.Component {
       messages: []
     }
   }
-  
+
+  handleChange(event){
+    this.setState({
+      input: event.target.value,
+      messages: this.state.messages
+    })
+  }
+
+  submitMessage(){
+    this.setState({
+      input: '',
+      messages: [...this.state.messages, this.state.input]
+    })
+  }
+
   render() {
-    return <div />
+    return (
+      <div>
+        <h2>Type in a new Message:</h2>
+        <input onChange={this.handleChange.bind(this)} value={this.state.input}/>
+        <button onClick={this.submitMessage.bind(this)}>Submit</button>
+        <ul>
+          {this.state.messages.map((x, i)=>{
+            return <li key={i}>{x}</li>
+          })}
+        </ul>
+      </div>
+    );
   }
 };
